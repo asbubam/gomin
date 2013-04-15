c86eea7e721325ea748c39a48e77be5011bc7411
asbubam <asbubam@gmail.com>
2013-04-15 17:26:08 +0900

    set base

---
+++ db/migrate/20130415073533_create_problems.rb
@@ -0,0 +1,12 @@
+class CreateProblems < ActiveRecord::Migration
+  def change
+    create_table :problems do |t|
+			t.string 	:title
+			t.text 		:content, :null => false
+			t.integer	:like, :null => false, :default => 0
+			t.integer :dislike, :null => false, :default => 0
+
+      t.timestamps
+    end
+  end
+end
